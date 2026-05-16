1b8f4a00c7756740216ccdcb4649846e111e83e8
cmillerdev <conleymiller0@gmail.com>
2026-05-16 15:11:29 -0400

    fix mobile bw filter

--- src/App.jsx
+++ src/App.jsx
@@ -551,8 +551,22 @@ export default function DisposableCamera() {
   
       const ctx = canvas.getContext("2d");
       ctx.drawImage(img, 0, 0);
-  
-      applySelectedFilter(ctx, canvas.width, canvas.height);
+
+if (filterType === "bw") {
+  const imageData = ctx.getImageData(0, 0, tempCanvas.width, tempCanvas.height);
+  const data = imageData.data;
+
+  for (let i = 0; i < data.length; i += 4) {
+    const gray = data[i] * 0.3 + data[i + 1] * 0.59 + data[i + 2] * 0.11;
+    data[i] = gray;
+    data[i + 1] = gray;
+    data[i + 2] = gray;
+  }
+
+  ctx.putImageData(imageData, 0, 0);
+}
+
+applyFilmEffect(ctx, tempCanvas.width, tempCanvas.height);
   
       const filteredDataUrl = canvas.toDataURL("image/jpeg", 0.88);
       addPhoto(filteredDataUrl, "image");
